ca180402c3bdbbd6312a6b24933a9dde2514e475
Mubarak Rocky <mubarakrocky@gmail.com>
2013-11-13 17:36:38 +0530

    Initial commit. Adding rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuzzoekTest::Application.config.secret_key_base = 'fcd1c2b7566d91ef9c8ce567cba36ea98772b3d17c95c38318ee0227d76d7150f1f031b34510ee7e1c5441708b20b6b7577cd48df6f5e50546e4699df9478ee8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BuzzoekTest::Application.config.session_store :cookie_store, key: '_buzzoek_test_session'
